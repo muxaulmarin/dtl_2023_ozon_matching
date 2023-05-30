420902c31ae84b29cf95dd1496ce277f2414ac58
Andrey Babkin <andrey.babkin.ru71@gmail.com>
2023-05-30 15:12:43 +0000

    docs

---
+++ final_solution/inference.sh
@@ -0,0 +1,40 @@
+# preprocessing
+python -m ozon_matching.andrey_solution preprocess \
+    --pairs-path data/raw/test_pairs_wo_target.parquet \
+    --products-path data/raw/test_data.parquet
+
+# feature engineering
+## stats
+python -m ozon_matching.andrey_solution generate-features \
+    --feature-type categories \
+    --feature-type characteristics \
+    --feature-type colors \
+    --feature-type names \
+    --feature-type pictures \
+    --feature-type variants \
+    --pairs-path data/preprocessed/test_pairs_wo_target.parquet \
+    --products-path data/preprocessed/test_data.parquet
+## tfidf
+python -m ozon_matching.andrey_solution create-tfidf-similarity-features \
+    --col-name characteristics_attributes \
+    --col-name characteristics \
+    --col-name name_norm_tokens \
+    --col-name name_tokens \
+    --col-name color_parsed \
+    --pairs-path data/preprocessed/test_pairs_wo_target.parquet \
+    --output-file test.parquet
+## Misha features
+### to be done
+## Sergey features
+### to be done
+
+# preparing dataset
+python -m ozon_matching.andrey_solution join-features \
+    --features-path data/features/ \
+    --pairs-path data/preprocessed/test_pairs_wo_target.parquet \
+    --output-file test.parquet
+
+# preparing submission
+python -m ozon_matching.andrey_solution prepare-submission \
+    --test-path data/dataset/test.parquet \
+    --experiment-path experiments/final
---
+++ final_solution/train.sh
@@ -0,0 +1,52 @@
+# preprocessing
+python -m ozon_matching.andrey_solution preprocess \
+    --pairs-path data/raw/train_pairs.parquet \
+    --products-path data/raw/train_data.parquet
+
+# feature engineering
+## stats
+python -m ozon_matching.andrey_solution generate-features \
+    --feature-type categories \
+    --feature-type characteristics \
+    --feature-type colors \
+    --feature-type names \
+    --feature-type pictures \
+    --feature-type variants \
+    --pairs-path data/preprocessed/train_pairs.parquet \
+    --products-path data/preprocessed/train_data.parquet
+## tfidf
+python -m ozon_matching.andrey_solution create-tfidf-matrix \
+    --col-name characteristics_attributes \
+    --col-name characteristics \
+    --col-name name_norm_tokens \
+    --col-name name_tokens \
+    --col-name color_parsed \
+    --test-products-path data/preprocessed/test_data.parquet \
+    --train-products-path data/preprocessed/train_data.parquet
+python -m ozon_matching.andrey_solution create-tfidf-similarity-features \
+    --col-name characteristics_attributes \
+    --col-name characteristics \
+    --col-name name_norm_tokens \
+    --col-name name_tokens \
+    --col-name color_parsed \
+    --pairs-path data/preprocessed/train_pairs.parquet \
+    --output-file train.parquet
+## Misha features
+### to be done
+## Sergey features
+### to be done
+
+# splits generation
+## to be done
+
+# preparing dataset
+python -m ozon_matching.andrey_solution join-features \
+    --features-path data/features/ \
+    --pairs-path data/preprocessed/train_pairs.parquet \
+    --output-file train.parquet
+
+# fit model
+python -m ozon_matching.andrey_solution fit-catboost \
+    --train-path data/dataset/train.parquet \
+    --experiment-path experiments/final \
+    --folds-path data/cv_pivot.parquet
